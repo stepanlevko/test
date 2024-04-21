620b54f73b66ad523fccd7ec6afa4798d3aacebc
stepanlevko <stepanlevko3@gmail.com>
2024-04-21 18:34:47 +0300

    Test

--- Homework_17.py
+++ Homework_17.py
@@ -1,1 +1,10 @@
+text = 'Hello world'
+value = 3.1415
+lis = (1, 4, 5, 'yes', -2, '2')
+func = 'a' + 'b'
+
+with open('text.txt', 'w') as f:
+    f.write('text = "Hello world"\n')
+    f.write('value = 3.1415\n')
+
 
