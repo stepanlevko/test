981d3ea046096967c8859a0fa6e508c4ea7196de
stepanlevko <stepanlevko3@gmail.com>
2024-04-21 18:31:24 +0300

    Initial commit

---
+++ lesson_10/01.py
@@ -0,0 +1,5 @@
+text = 'Hello world'
+value = 3.1415
+l = (1, 4, 5, 'yes', -2, '2')
+func = 'a' + 'b'
+print(text, value, l, func)
--- lesson_10/Homework_17.py
+++ lesson_10/Homework_17.py
@@ -1,2 +1,10 @@
-home
+text = 'Hello world'
+value = 3.1415
+lis = (1, 4, 5, 'yes', -2, '2')
+func = 'a' + 'b'
+
+with open('text.txt', 'w') as f:
+    f.write('text = "Hello world"\n')
+    f.write('value = 3.1415\n')
+
 
