d16b7ffb3c4369f2ff853c3498c3964c60fa905a
stepanlevko <stepanlevko3@gmail.com>
2024-04-28 12:07:05 +0300

    Test

---
+++ Homework/123.py
@@ -0,0 +1,25 @@
+import time
+
+def get_data():
+    def get_cpu(data):
+
+        return data
+    def get_memory(data):
+
+        return data
+
+    def
+    data = {}
+
+
+
+def main():
+    while True:
+        data = get_data()
+        new_data = prepare_data(data)
+        print_data(new_data)
+        time.sleep(2)
+
+if __name__ == '__main__':
+    main()
+
