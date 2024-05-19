541d6bd7ee7c6b80f9634e8d9164e546bf3c9602
stepanlevko <stepanlevko3@gmail.com>
2024-05-19 19:47:37 +0300

    Test

--- Diplom/main.py
+++ Diplom/main.py
@@ -11,34 +11,21 @@ while True:
             self.deathday = str(deathday)
 
         def full_year(self):
+            self.birthday = re.split('[/. -]', self.birthday)
+            birthday_year, birthday_month, birthday_day = int(self.birthday[2]), int(self.birthday[1]), int(
+                self.birthday[0])
             if self.deathday != '':
-                self.birthday = re.split('[/. -]', self.birthday)
                 self.deathday = re.split('[/. -]', self.deathday)
-                birthday_year = int(self.birthday[2])
-                birthday_month = int(self.birthday[1])
-                birthday_day = int(self.birthday[0])
-                deathday_year = int(self.deathday[2])
-                deathday_month = int(self.deathday[1])
-                deathday_day = int(self.deathday[0])
-                years = deathday_year - birthday_year
-                if birthday_month > deathday_month:
-                    years -= 1
-                elif birthday_month == deathday_month:
-                    if birthday_day > deathday_day:
-                        years -= 1
-                # return years
-            else:
+                year, month, day = int(self.deathday[2]), int(self.deathday[1]), int(self.deathday[0])
+            elif self.deathday == '':
                 now = datetime.date.today()
-                self.birthday = re.split('[/. -]', self.birthday)
-                birthday_year = int(self.birthday[2])
-                birthday_month = int(self.birthday[1])
-                birthday_day = int(self.birthday[0])
-                years = now.year - birthday_year
-                if birthday_month > now.month:
+                year, month, day = now.year, now.month, now.day
+            years = year - birthday_year
+            if birthday_month > month:
+                years -= 1
+            elif birthday_month == month:
+                if birthday_day > day:
                     years -= 1
-                elif birthday_month == now.month:
-                    if birthday_day > now.day:
-                        years -= 1
             return years
 
         def data_read(self):
@@ -60,38 +47,40 @@ while True:
                         res.append((k, v))
                 return res
             print(res)
+    try:
+        poz = int(input('1 - завантаження бази даних, 2 - введення даних вручну, '
+                        '3 - пошук особи. Введіть необхідне число: '))
 
-    poz = int(input('1 - завантаження бази даних, 2 - введення даних вручну, '
-                    '3 - пошук особи. Введіть необхідне число: '))
-
-    if poz == 1:
-        with open('file_json.json', encoding='utf-8') as file:
-            data = json.load(file)
-        print(data)
-
-    elif poz == 2:
-        name = (input('Введіть ім\'я: ').title())
-        surname = (input('Введіть прізвище: ').title())
-        secondname = (input('Введіть пo батькові: ').title())
-        birthday = str(input('Введіть дату народження у форматі д/м/рік: '))
-        deathday = str(input('Введіть дату смерті (якшо є) у форматі д/м/рік: '))
-        sex = str((input('Виберыть вашу стать (необовязково) у форматі: [M] - чол, [F] - жін, '': ')).upper())
-        full = Human(name=name, birthday=birthday, deathday=deathday)
-        data = {
-            'name': name,
-            'surname': surname,
-            'secondname': secondname,
-            'birthday': birthday,
-            'deathday': deathday,
-            'sex': sex,
-            'full year':  full.full_year()
-        }
-        print(data)
-        Human.data_save(data)
-
-    elif poz == 3:
-        print(Human.find_names(user=input('Кого шукаєте?: ').title()))
-
-    elif poz != 1 and 2 and 3 and poz is not int():
-        print('Введеного числа немає у списку варіантів. Спробуйте знову')
+        if poz == 1:
+            with open('file_json.json', encoding='utf-8') as file:
+                data = json.load(file)
+            print(data)
+        elif poz == 2:
+            try:
+                name = (input('Введіть ім\'я: ').title())
+                surname = (input('Введіть прізвище: ').title())
+                secondname = (input('Введіть пo батькові: ').title())
+                birthday = str(input('Введіть дату народження у форматі д/м/рік: '))
+                deathday = str(input('Введіть дату смерті (якшо є) у форматі д/м/рік: '))
+                sex = str((input('Виберіть вашу стать (необов\'язково) у форматі: [M] - чол, [F] - жін, '': ')).upper())
+                full = Human(name=name, birthday=birthday, deathday=deathday)
+                data = {
+                    'name': name,
+                    'surname': surname,
+                    'secondname': secondname,
+                    'birthday': birthday,
+                    'deathday': deathday,
+                    'sex': sex,
+                    'full year':  full.full_year()
+                }
+                print(data)
+                Human.data_save(data)
+            except IndexError:
+                print('Введіть коректне значення')
+        elif poz == 3:
+            print(Human.find_names(user=input('Кого шукаєте?: ').title()))
+        elif poz != 1 and 2 and 3 and poz is not int():
+            print('Введеного числа немає у списку варіантів. Спробуйте знову')
+    except ValueError:
+        print('Введіть коректне значення')
 pass
